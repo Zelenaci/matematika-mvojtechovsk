47ffe62daef8caa475c46d357bdb933fa331ec89
Michal <voj38077@spseol.cz>
2022-01-17 09:35:48 +0100

    zacatek

--- main.py
+++ main.py
@@ -2,36 +2,57 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-
+from tkinter import *
+import random
 # from tkinter import ttk
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
 
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "Matematika"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
+        self.lbl = tk.Label(self, text="Matematika")
         self.lbl.pack()
+        self.generuj()
+        self.entry = Entry(self,width = 6)
+        self.entry.pack()
+        #self.entry.grid(row=2,column = 3)
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
+        
+
+
+    def generuj(self):
+        self.funkce = random.choice([self.plus, self.deleno, self.krat, self.minus])
+        self.label = Label(self, text=1)
+        self.label.pack(LEFT)
+
+    def plus(self):
+        self.cisloA = random.randint(1, 99)
+        self.cisloB = random.randint(1, 100-self.cisloA) 
+        self.vysledek = self.cisloA + self.cisloB
+        self.lbl.config(text="+")
+
+    def deleno(self):
+        self.cisloA = random.randint(1, 10)
+        self.vysledek =random.randint(1, 10)
+        self.cisloB = self.cisloB * self.vysledek
+        self.lbl.config(text="/")
+    def minus(self):
+        self.cisloA = random.randint(1, 99)
+        self.cisloB = random.randint(1, self.cisloA) 
+        self.vysledek = self.cisloA - self.cisloB
+        self.lbl.config(text="-")
+
+    def krat(self):
+        self.cisloA = random.randint(1, 10)
+        self.cisloB = random.randint(1, 10) 
+        self.vysledek = self.cisloA * self.cisloB
+        self.lbl.config(text="*")   
 
     def about(self):
         window = About(self)
