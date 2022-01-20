d4824cb7e845560bee2155ebc977a48ccb47d8ef
Michal Vojtěchovský <voj38077@spseol.cz>
2022-01-20 19:00:09 +0100

    generátor přííkladů

--- main.py
+++ main.py
@@ -24,39 +24,43 @@ class Application(tk.Tk):
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
         
+        
 
 
     def generuj(self):
-        self.funkce = random.choice([self.plus, self.deleno, self.krat, self.minus])
-        self.label = Label(self, text=1)
-        self.label.pack(LEFT)
-
+        self.funkce = random.choice([self.plus,self.deleno,self.minus,self.krat])
+        self.priklad = self.funkce()
+        self.lbl.config(text= self.priklad)
+        self.lbl.pack()
+        print(self.priklad)
+        
     def plus(self):
         self.cisloA = random.randint(1, 99)
         self.cisloB = random.randint(1, 100-self.cisloA) 
         self.vysledek = self.cisloA + self.cisloB
-        self.lbl.config(text="+")
-
+        self.znamenko = " + "
+        return f"{self.cisloA}{self.znamenko}{self.cisloB}"
+   
     def deleno(self):
         self.cisloA = random.randint(1, 10)
         self.vysledek =random.randint(1, 10)
-        self.cisloB = self.cisloB * self.vysledek
-        self.lbl.config(text="/")
+        self.cisloB = self.cisloA * self.vysledek
+        self.znamenko = " / "
+        return f"{self.cisloB}{self.znamenko}{self.cisloA}"
+
     def minus(self):
         self.cisloA = random.randint(1, 99)
         self.cisloB = random.randint(1, self.cisloA) 
         self.vysledek = self.cisloA - self.cisloB
-        self.lbl.config(text="-")
+        self.znamenko = " - "
+        return f"{self.cisloA}{self.znamenko}{self.cisloB}"
 
     def krat(self):
         self.cisloA = random.randint(1, 10)
         self.cisloB = random.randint(1, 10) 
         self.vysledek = self.cisloA * self.cisloB
-        self.lbl.config(text="*")   
-
-    def about(self):
-        window = About(self)
-        window.grab_set()
+        self.znamenko = " * "
+        return f"{self.cisloA}{self.znamenko}{self.cisloB}"  
 
     def quit(self, event=None):
         super().quit()
